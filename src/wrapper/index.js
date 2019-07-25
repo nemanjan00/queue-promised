fb90ee5adfbfe3b297a0242b0d2fd6ecc8c9fce0
nemanjan00 <nemanjan00@gmail.com>
2019-07-25 19:08:26 +0200

    Wrapper

--- src/wrapper/index.js
+++ src/wrapper/index.js
@@ -4,12 +4,22 @@ const worker = require("../worker");
 const uuid = require("uuid/v4");
 
 module.exports = (func, count) => {
+	if(count === undefined) {
+		count = 1;
+	}
+
 	const id = uuid();
 
 	Array(count).fill(1).map(() => worker(id, func));
 
-	return (...args) => {
+	const resp = (...args) => {
 		return queue.push(id, ...args);
 	};
+
+	resp.addWorkers = (count) => {
+		Array(count).fill(1).map(() => worker(id, func));
+	};
+
+	return resp;
 };
 
