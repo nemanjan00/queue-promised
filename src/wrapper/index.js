b9287b116a72430ffa8ab32e76811d072ecbcb29
nemanjan00 <nemanjan00@gmail.com>
2019-08-01 13:51:21 +0200

    Add timeout, minimum time to return after and calls per second rate limiting

--- src/wrapper/index.js
+++ src/wrapper/index.js
@@ -1,23 +1,73 @@
+const _ = require("lodash");
+
 const queue = require("../queue");
 const worker = require("../worker");
 
 const uuid = require("uuid/v4");
 
-module.exports = (func, count) => {
-	if(count === undefined) {
-		count = 1;
-	}
+const timeout = (time) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => reject("timeout"), time);
+	});
+};
 
+module.exports = (func, options) => {
 	const id = uuid();
 
-	Array(count).fill(1).map(() => worker(id, func));
+	// Configuration
+	if(options === undefined) {
+		options = {
+			count: 1
+		};
+	}
 
+	if(typeof options == "number") {
+		options = {
+			count: options
+		};
+	}
+
+	// Configure workers
+	if(options.maxTime !== undefined) {
+		const oldFunc = func;
+
+		func = (...args) => {
+			return Promise.race([
+				oldFunc(...args),
+				timeout(options.maxTime)
+			]);
+		};
+	}
+
+	if(options.rateLimit !== undefined) {
+		options.minTime = (1000 * options.count) / options.count;
+	}
+
+	if(options.minTime !== undefined) {
+		const oldFunc = func;
+
+		func = (...args) => {
+			return new Promise((resolve, reject) => {
+				const promise = oldFunc(...args);
+
+				timeout(options.minTime).catch(() => {
+					promise.then(resolve).catch(reject);
+				});
+			});
+		};
+	}
+
+	// Init workers
+	_.times(options.count, () => worker(id, func));
+
+	// Logic for adding tasks
 	const resp = (...args) => {
 		return queue.push(id, ...args);
 	};
 
+	// Exposed logic
 	resp.addWorkers = (count) => {
-		Array(count).fill(1).map(() => worker(id, func));
+		_.times(count, () => worker(id, func));
 	};
 
 	return resp;
